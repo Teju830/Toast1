a5d5054df3116f74393b54fe70683e88c38013fb
Teju830 <tejakumari555@gmail.com>
2020-09-11 12:55:39 +0530

    Task3

---
+++ app/src/main/java/com/example/workshop1/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.workshop1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView mTextview;
+    int count=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextview=findViewById(R.id.tv);
+        if(savedInstanceState!=null && savedInstanceState.containsKey("ap")){
+            count=savedInstanceState.getInt("ap");
+            mTextview.setText(String.valueOf(count));
+        }
+
+    }
+
+    public void showToast(View view) {
+        Toast.makeText(this, "Welcome to app development", Toast.LENGTH_SHORT).show();
+    }
+
+    public void showCount(View view) {
+        count--;
+        mTextview.setText(String.valueOf(count));
+        Toast.makeText(getApplicationContext(),String.valueOf(count), Toast.LENGTH_LONG).show();
+
+
+    }
+    public void showCount1(View view) {
+        count++;
+        mTextview.setText(String.valueOf(count));
+        Toast.makeText(getApplicationContext(),String.valueOf(count), Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("ap",count);
+    }
+}
+
